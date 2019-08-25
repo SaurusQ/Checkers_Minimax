5245b5af2f7eafd2085ca502801bf5ac0d45843d
sauramj2 <joona.sauramaki@aalto.fi>
2019-08-25 15:26:22 +0300

    Added more methods (cross referencing need to be solved)

--- square.hpp
+++ square.hpp
@@ -9,6 +9,14 @@
 #define S_ENEMY         '2'
 #define S_ENEMY_K       '4'
 
+#include <vector>
+#include <exception>
+
+#include "utils.hpp"
+#include "move.hpp"
+
+class Board;
+
 enum SquareType {
     EMPTY,
     FRIENDLY,
@@ -18,19 +26,29 @@ enum SquareType {
 class Square
 {
     public:
+        Square& operator=(const Square &another);
+
+        std::vector<Move> getPossibleMoves(Board *board, Pos relChainLoc = {0, 0});
         void setSquare(SquareType type, bool king = false);
+
         bool isKing() { return king_; };
         void throne() { king_ = true; };
         void deThrone() { king_ = true; };
+
+        bool isEnemy(const Square &another);
         
-        void setEnemy() { type_ = SquareType::ENEMY; };
-        void setFriendly() { type_ = SquareType::ENEMY; };
+        void setEnemy(bool king = false) { type_ = SquareType::ENEMY; king_ = king; };
+        void setFriendly(bool king = false) { type_ = SquareType::FRIENDLY; king_ = king; };
+        void setEmpty() { type_ = SquareType::EMPTY; king_ = false; };
+        void setPos(Pos pos) { pos_ = pos; };
         
+        SquareType getType() const { return type_; };
+        Pos getPos() const { return pos_; };
         char getSymbol();
     private:
+        Pos pos_;
         bool king_ = false;
-        SquareType type_ = EMPTY;
-
+        SquareType type_ = SquareType::EMPTY;
 };
 
 #endif
