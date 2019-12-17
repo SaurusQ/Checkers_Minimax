59792cfc00f6383f35bb71e31acb75eb8c59808f
SaurusQ <53377378+SaurusQ@users.noreply.github.com>
2019-12-17 23:19:54 +0200

    Board initialization and print working

--- board.cpp
+++ board.cpp
@@ -5,7 +5,23 @@ Board::Board()
     : reds_(0)
     , blacks_(0)
 {
-    
+    //Set board completely empty
+    std::memset(grid_, 0, sizeof(grid_) * sizeof(grid_[0]));
+    //Set blacks up
+    for(int i = 0; i < 3 * SEGMENT + 2; i++)
+    {
+        grid_[i] = BLACK_PAWN;
+    }
+    //Set reds down
+    for(int i = GRID_SIZE - (3 * SEGMENT + 2); i < GRID_SIZE; i++)
+    {
+        grid_[i] = RED_PAWN;
+    }
+    //Set blocks
+    for(int i = SEGMENT; i < GRID_SIZE; i += 2 * SEGMENT + 1)
+    {
+        grid_[i] = BLOCKED;
+    }
 }
 
 void Board::calculateMoves(bool red)
@@ -43,7 +59,7 @@ void Board::moveUp(int i)
 void Board::moveDown(int i)
 {
     //Can't move any more down
-    if(i > BOARD_SIZE - SEGMENT) return;
+    if(i > GRID_SIZE - SEGMENT) return;
 
     //Left
     if(grid_[i + SEGMENT] == EMPTY)
@@ -51,4 +67,36 @@ void Board::moveDown(int i)
     //Right
     if(grid_[i + 1 + SEGMENT] == EMPTY)
         moves_.emplace_back(Move(i, i + 1 + SEGMENT));
+}
+
+std::ostream& operator<<(std::ostream& os, const Board& b)
+{
+    for(int i = 0; i < GRID_SIZE; i++)
+    {
+        switch (b.grid_[i])
+        {
+            case EMPTY:
+                os << "  ";
+                break;
+            case RED_PAWN:
+                os << "R ";
+                break;
+            case RED_KING:
+                os << "E ";
+                break;
+            case BLACK_PAWN:
+                os << "B ";
+                break;
+            case BLACK_KING:
+                os << "L ";
+                break;
+            case BLOCKED:
+                os << "\n ";
+            default:
+                break;
+        }
+        if((i + 1) % (2 * SEGMENT + 1) == 0) os << "\n";
+    }
+    os << std::endl;
+    return os;
 }
---
+++ definitions.hpp
@@ -0,0 +1,23 @@
+
+#ifndef DEFINITIONS_HPP
+#define DEFINITIONS_HPP
+
+#define BOARD_SIZE  8
+#define SEGMENT     BOARD_SIZE / 2
+#define GRID_SIZE   BOARD_SIZE * BOARD_SIZE / 2 + SEGMENT
+
+#define IS_RED   0x02
+#define IS_BLACK 0x01
+#define IS_KING  0x04
+
+enum Piece
+{
+    EMPTY       = 0, //0000
+    BLACK_PAWN  = 1, //0001
+    BLACK_KING  = 5, //0101
+    RED_PAWN    = 2, //0010
+    RED_KING    = 6, //0110
+    BLOCKED     = 8  //1000
+};
+
+#endif
