a5eec6d53c5c9ec68effb8ef283f789f2457a201
SaurusQ <53377378+SaurusQ@users.noreply.github.com>
2019-12-25 16:23:37 +0200

    Start moves working

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <thread>
 
 #include "board.hpp"
 
@@ -7,6 +8,21 @@ int main()
 {
     Board board;
     std::cout << board;
+    board.calculateMoves(IS_BLACK);
+
+    for(auto move : board.getMoves())
+    {
+        std::cout << move;
+    }
+
+    for(auto move : board.getMoves())
+    {
+        std::cout << move;
+        move.execute(board.getGrid());
+        std::cout << board;
+        move.undo(board.getGrid());
+        std::cout << board;
+    }
 
     while(1){}
     return 0;
