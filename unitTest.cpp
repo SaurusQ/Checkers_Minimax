f68b00384f05afd067e1036e7ae8172402d78b92
sauramj2 <joona.sauramaki@aalto.fi>
2020-01-08 20:18:30 +0200

    Grid conversion failing

--- unitTest.cpp
+++ unitTest.cpp
@@ -1,9 +1,13 @@
 
+#include <iostream>
+
 #include "board.hpp"
 
 bool unitTest()
 {
-    uint8_t grid1[8][8] = {
+    std::cout << "Test 1" << std::endl;
+
+    grid_t grid1[8][8] = {
         {0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 1, 0, 0, 0, 0, 0},
@@ -13,8 +17,21 @@ bool unitTest()
         {0, 0, 0, 0, 0, 0, 0, 0}
     };
 
-    Board test1((uint8_t**)grid1);
+    Board test1(grid1);
+    std::cout << test1 << std::endl;
+    
+    std::cout << "Red moves" << std::endl;
     test1.calculateMoves(IS_RED);
+    for(auto move : test1.getMoves())
+    {
+        std::cout << move << std::endl;
+    }
+
+    std::cout << "Black moves" << std::endl;
     test1.calculateMoves(IS_BLACK);
+    for(auto move : test1.getMoves())
+    {
+        std::cout << move << std::endl;
+    }
     return true;
 }
