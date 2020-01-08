4e1ef322b61ae5c2cf2ae75fa884df15bb95f16d
SaurusQ <53377378+SaurusQ@users.noreply.github.com>
2020-01-08 22:28:21 +0200

    Test 5 successful multipath eating working

--- unitTest.cpp
+++ unitTest.cpp
@@ -5,34 +5,146 @@
 
 bool unitTest()
 {
-    std::cout << "Test 1" << std::endl;
+    /*{
+        std::cout << "Test 1" << std::endl;
 
-    grid_t grid1[8][8] = {
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 1, 0, 0, 0, 0, 0},
-        {0, 0, 0, 2, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0}
-    };
+        grid_t grid[8][8] = {
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 2, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0}
+        };
 
-    Board test1(grid1);
-    std::cout << test1 << std::endl;
+        Board test(grid);
+        std::cout << test << std::endl;
+        
+        std::cout << "Red moves" << std::endl;
+        test.calculateMoves(IS_RED);
+        for(auto move : test.getMoves())
+        {
+            std::cout << move << std::endl;
+        }
+        auto moves = test.getMoves();
+        if(moves.size() != 1) return false;
+        if(moves[0].getStart() != 15 || moves[0].getEnd() != 5) return false;
+
+        std::cout << "Black moves" << std::endl;
+        test.calculateMoves(IS_BLACK);
+        for(auto move : test.getMoves())
+        {
+            std::cout << move << std::endl;
+        }
+        moves = test.getMoves();
+        if(moves.size() != 1) return false;
+        if(moves[0].getStart() != 10 || moves[0].getEnd() != 20) return false;
+    }
     
-    std::cout << "Red moves" << std::endl;
-    test1.calculateMoves(IS_RED);
-    for(auto move : test1.getMoves())
     {
-        std::cout << move << std::endl;
+        std::cout << "Test 2" << std::endl;
+
+        grid_t grid[8][8] = {
+            {0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 2, 0, 2, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 2, 0, 0, 0, 2, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 2, 0, 2, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        Board test(grid);
+        std::cout << test << std::endl;
+        
+        std::cout << "Eats" << std::endl;
+        test.calculateMoves(IS_BLACK);
+        for(auto move : test.getMoves())
+        {
+            if(move.getStart() != 1 || move.getEnd() != 29) return false;
+            std::cout << move;
+        }
     }
 
-    std::cout << "Black moves" << std::endl;
-    test1.calculateMoves(IS_BLACK);
-    for(auto move : test1.getMoves())
     {
-        std::cout << move << std::endl;
+        std::cout << "Test 3" << std::endl;
+
+        grid_t grid[8][8] = {
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 2, 0, 2, 0, 2, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 2, 0, 2, 0, 2, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 2, 0, 2, 0, 2, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        Board test(grid);
+        std::cout << test << std::endl;
+        
+        std::cout << "Eats" << std::endl;
+        test.calculateMoves(IS_BLACK);
+        for(auto move : test.getMoves())
+        {
+            if(move.getEnd() != 28 && move.getEnd() != 30) return false;
+            std::cout << move;
+        }
     }
+
+    {
+        std::cout << "Test 4" << std::endl;
+
+        grid_t grid[8][8] = {
+            {1, 0, 2, 0, 2, 0, 2, 0},
+            {0, 2, 0, 2, 0, 0, 0, 0},
+            {2, 0, 2, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 1, 0, 0, 0, 1, 0, 1}
+        };
+
+        Board test(grid);
+        std::cout << test << std::endl;
+        
+        test.calculateMoves(IS_BLACK);
+        std::cout << "#Moves: " << test.getMoves().size() << std::endl;
+        for(auto move : test.getMoves())
+        {
+            std::cout << move;
+        }
+        if(test.getMoves().size() != 0) return false;
+    }
+*/
+    {
+        std::cout << "Test 5" << std::endl;
+
+        grid_t grid[8][8] = {
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 2, 0, 2, 0, 2, 0, 0},
+            {0, 0, 3, 0, 0, 0, 0, 0},
+            {0, 2, 0, 2, 0, 2, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 2, 0, 2, 0, 2, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        Board test(grid);
+        std::cout << test << std::endl;
+        
+        std::cout << "Eats" << std::endl;
+        test.calculateMoves(IS_BLACK);
+        for(auto move : test.getMoves())
+        {
+            std::cout << move;
+        }
+    }
+
     return true;
 }
