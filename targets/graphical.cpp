02691d07d044f91649f8858e1678d019e42b8b7a
Saurus <313.saurus@gmail.com>
2021-09-07 22:41:17 +0300

    Change colors

--- targets/graphical.cpp
+++ targets/graphical.cpp
@@ -9,7 +9,7 @@
 
 #define BOARD_GRAPH_SIZE    800 // Must be divisible by BOARD_SIZE
 #define RECTANGLE_SIZE      (BOARD_GRAPH_SIZE / BOARD_SIZE)
-#define PIECE_SIZE          (RECTANGLE_SIZE * 0.45f)
+#define PIECE_SIZE          (RECTANGLE_SIZE * 0.40f)
 #define PIECE_OFFSET        ((RECTANGLE_SIZE / 2.0f) - PIECE_SIZE)
 
 struct PieceData
@@ -21,25 +21,19 @@ struct PieceData
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(BOARD_GRAPH_SIZE, BOARD_GRAPH_SIZE), "Checkers MiniMax");
+    sf::RenderWindow window(sf::VideoMode(BOARD_GRAPH_SIZE, BOARD_GRAPH_SIZE), "Checkers MiniMax", sf::Style::Close);
 
     // Board
     sf::RectangleShape blackRectangle(sf::Vector2f(RECTANGLE_SIZE, RECTANGLE_SIZE));
-    blackRectangle.setFillColor(sf::Color::Black);
+    blackRectangle.setFillColor(sf::Color(59, 46, 15));
     sf::RectangleShape whiteRectangle(sf::Vector2f(RECTANGLE_SIZE, RECTANGLE_SIZE));
-    whiteRectangle.setFillColor(sf::Color::White);
-
-    // Pieces
-    sf::CircleShape blackPiece(PIECE_SIZE);
-    blackPiece.setFillColor(sf::Color::Blue);
-    sf::CircleShape redPiece(PIECE_SIZE);
-    redPiece.setFillColor(sf::Color::Red);
+    whiteRectangle.setFillColor(sf::Color(120, 98, 47));
     
-    auto solver = std::make_unique<MiniMaxAB>(1);
+    auto solver = std::make_unique<MiniMaxAB>(9);
     Game game(solver.get(), GameType::CPU_VS_PLAYER);
 
-    std::vector<PieceData> blackPieces(game.getBoard().aliveBlacks(), PieceData{blackPiece, 0, 0});
-    std::vector<PieceData> redPieces(game.getBoard().aliveReds(), PieceData{redPiece, 0, 0});
+    std::vector<PieceData> pieces;
+    pieces.reserve(game.getBoard().aliveBlacks() + game.getBoard().aliveReds());
 
     bool updatePieces = true;
     bool movePiece = false;
@@ -61,20 +55,11 @@ int main()
             // Drag pieces
             if(e.type == sf::Event::MouseButtonPressed)
             {
-                for(auto& b : blackPieces)
+                for(auto& p : pieces)
                 {
-                    if(b.shape.getGlobalBounds().contains(mousePos.x, mousePos.y))
+                    if(p.shape.getGlobalBounds().contains(mousePos.x, mousePos.y))
                     {
-                        pieceToMove = &b;
-                        movePiece = true;
-                        break;
-                    }
-                }
-                for(auto& r : redPieces)
-                {
-                    if(r.shape.getGlobalBounds().contains(mousePos.x, mousePos.y))
-                    {
-                        pieceToMove = &r;
+                        pieceToMove = &p;
                         movePiece = true;
                         break;
                     }
@@ -142,8 +127,7 @@ int main()
         if(updatePieces)
         {
             updatePieces = false;
-            blackPieces.clear();
-            redPieces.clear();
+            pieces.clear();
             game.nextTurn();
             const Board b = game.getBoard();
             std::cout << b;
@@ -152,15 +136,20 @@ int main()
                 for(int y = 0; y < BOARD_SIZE; y++)
                 {
                     uint8_t piece = b.getLocation(x, y);
-                    if(piece & IS_BLACK)
+                    if(piece & IS_PIECE)
                     {
-                        blackPiece.setPosition(x * RECTANGLE_SIZE + PIECE_OFFSET, y * RECTANGLE_SIZE + PIECE_OFFSET);
-                        blackPieces.push_back(PieceData{blackPiece, x, y});
-                    }
-                    else if(piece & IS_RED)
-                    {
-                        redPiece.setPosition(x * RECTANGLE_SIZE + PIECE_OFFSET, y * RECTANGLE_SIZE + PIECE_OFFSET);
-                        redPieces.push_back(PieceData{redPiece, x, y});
+                        PieceData pd = {sf::CircleShape(PIECE_SIZE), x, y};
+                        pd.shape.setPosition(x * RECTANGLE_SIZE + PIECE_OFFSET, y * RECTANGLE_SIZE + PIECE_OFFSET);
+                        if(piece & IS_BLACK)
+                            pd.shape.setFillColor(sf::Color(79, 96, 184));
+                        if(piece & IS_RED)
+                            pd.shape.setFillColor(sf::Color(158, 49, 49));
+                        if(piece & IS_KING)
+                        {
+                            pd.shape.setOutlineColor(sf::Color(191, 175, 33));
+                            pd.shape.setOutlineThickness(3);
+                        }
+                        pieces.push_back(pd);
                     }
                 }
             }
@@ -179,13 +168,9 @@ int main()
             }
         }
         // Pieces
-        for(auto& b : blackPieces)
+        for(auto& p : pieces)
         {
-            window.draw(b.shape);
-        }
-        for(auto& r : redPieces)
-        {
-            window.draw(r.shape);
+            window.draw(p.shape);
         }
         // Update the window
         window.display();
